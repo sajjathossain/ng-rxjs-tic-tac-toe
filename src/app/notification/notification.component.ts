1d4322d163cbc544aecb1f1786d8502ae4844415
sajjathossain <sajjathossain.official@gmail.com>
2025-02-13 12:49:34 +0600

    feat(app): create notification component

---
+++ src/app/notification/notification.component.ts
@@ -0,0 +1,20 @@
+import { Component, inject } from '@angular/core';
+import { BoardService } from '../board.service';
+import { AsyncPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-notification',
+  standalone: true,
+  imports: [AsyncPipe],
+  styles: `
+    :host {
+      @apply absolute w-full h-full top-0 left-0 z-10 bg-gray-700 backdrop-blur-md
+    }
+  `,
+  templateUrl: './notification.component.html',
+})
+export class NotificationComponent {
+
+  protected readonly bs = inject(BoardService)
+
+}
