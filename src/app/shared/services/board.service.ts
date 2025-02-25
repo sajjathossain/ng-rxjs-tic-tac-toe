5a40225bd74f6d3e1fad40b7205724259ab307da
sajjathossain <sajjathossain.official@gmail.com>
2025-02-25 08:09:14 +0600

    chore(board.service): clean up

--- src/app/shared/services/board.service.ts
+++ src/app/shared/services/board.service.ts
@@ -1,5 +1,6 @@
 import { TPlayer, TState } from "#shared/types";
-import { Injectable, signal } from "@angular/core";
+import { Injectable } from "@angular/core";
+import { toSignal } from "@angular/core/rxjs-interop";
 import { BehaviorSubject, combineLatest, fromEvent, Subject } from "rxjs";
 import { filter, map, mergeWith, scan, shareReplay, skipWhile, tap } from "rxjs/operators";
 
@@ -30,18 +31,11 @@ export class BoardService {
     return false
   }
 
-  private changePlayer() {
-    this.currentPlayer.update((currentPlayer) => currentPlayer === "X" ? "O" : "X");
-  }
+  private readonly addValue = (idx: number) => (state: TState): TState => {
+    const currentPlayer = this.isXNext() ? "X" : "O";
+    state.splice(idx, 1, currentPlayer);
 
-  private readonly addValue = (payload: { idx: number, player: TPlayer }) => (state: TState): TState => {
-    const { idx, player } = payload;
-    const currentValues = state;
-    const currentPlayer = player;
-    currentValues.splice(idx, 1, currentPlayer);
-    this.changePlayer()
-
-    return currentValues;
+    return state;
   }
 
   private get initialState(): TState {
@@ -49,8 +43,8 @@ export class BoardService {
     return initalState
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  private readonly resetBoard = (_event: void) => (_state: TState): TState => {
+
+  private readonly resetBoard = () => (): TState => {
     return this.initialState
   }
 
@@ -67,7 +61,7 @@ export class BoardService {
   private readonly keypress = (event: KeyboardEvent) => (state: TState): TState => {
     let key = Number(event.key);
     if (key === 0) {
-      return this.resetBoard()(state)
+      return this.resetBoard()()
     }
 
     key = key - 1
@@ -77,9 +71,7 @@ export class BoardService {
       return state
     }
 
-    state.splice(key, 1, this.currentPlayer())
-    this.changePlayer()
-    return state
+    return this.addValue(key)(state)
   }
 
   get board() {
@@ -90,14 +82,12 @@ export class BoardService {
     return state.filter(Boolean).length
   }
 
-  readonly currentPlayer = signal<TPlayer>("X");
   readonly resetBoard$ = new Subject<void>()
-  readonly addValue$ = new Subject<{ idx: number, player: TPlayer }>()
+  readonly addValue$ = new Subject<number>()
   readonly keypress$ = fromEvent<KeyboardEvent>(document, "keydown").pipe<KeyboardEvent, KeyboardEvent>(filter(this.isKeyAllowed), tap(event => event.preventDefault()))
 
   private readonly board$ = new BehaviorSubject<TState>(this.initialState)
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    .pipe(map((values) => (_state: TState) => values))
+    .pipe(map(() => (_state: TState) => _state))
     .pipe(
       mergeWith(
         this.keypress$.pipe(map(this.keypress)),
@@ -109,6 +99,10 @@ export class BoardService {
       scan((state, stateHandlerFN) => stateHandlerFN(state), this.initialState),
     ).pipe(shareReplay(1))
 
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  private readonly isXNext$ = new BehaviorSubject<boolean>(true).pipe(mergeWith(this.board$)).pipe(scan((_prev, _current) => !_prev, true))
+  readonly isXNext = toSignal(this.isXNext$)
 
   readonly totalCount$ = this.board$.pipe(
     map(this.caclulateTotalCount),
@@ -122,7 +116,7 @@ export class BoardService {
   )
   readonly isOWinner$ = this.board$.pipe(
     skipWhile(state => this.caclulateTotalCount(state) <= 4),
-    map(values => this.checkWinner(values, "O")),
+    scan((_prev, current) => this.checkWinner(current, "O"), false),
     shareReplay(1),
   )
 
