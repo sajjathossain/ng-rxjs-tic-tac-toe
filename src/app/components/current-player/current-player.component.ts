5afc0fb9245649f2a9a8c60ea136d0b32df6749a
sajjathossain <sajjathossain.official@gmail.com>
2025-03-02 23:00:38 +0600

    fix(components/current-player): fix current player indicator using async pipe and ng container

--- src/app/components/current-player/current-player.component.ts
+++ src/app/components/current-player/current-player.component.ts
@@ -1,17 +1,15 @@
 import { BoardService } from '#shared/services';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, NgIf } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-current-player',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
+  imports: [AsyncPipe, NgIf],
   providers: [AsyncPipe],
   templateUrl: './current-player.component.html',
 })
 export class CurrentPlayerComponent {
   readonly bs = inject(BoardService)
-  readonly ap = inject(AsyncPipe)
-  readonly currentPlayer = this.bs.isXNext() ? "X" : "O"
 }
